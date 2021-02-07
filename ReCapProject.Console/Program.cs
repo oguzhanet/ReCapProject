4b74c4b8bf446913d4d848c5e4ec7eeb3982482a
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-02-07 14:02:32 +0300

    7.02.2021

--- ReCapProject.Console/Program.cs
+++ ReCapProject.Console/Program.cs
@@ -12,42 +12,45 @@ namespace ReCapProject.Console
         {
             CarManager carManager = new CarManager(new EfCarDal());
 
-            
-
-            //carManager.Add(new Car
-            //{
-            //    BrandId = 2,
-            //    ColorId = 3,
-            //    Description = "Fiat",
-            //    DailyPrice = 120,
-            //    ModelYear = 2015
-            //});
-
-            //carManager.Update(new Car
-            //{
-            //    CarId = 3,
-            //    BrandId = 2,
-            //    ColorId = 1,
-            //    Description = "Reno",
-            //    DailyPrice = 120,
-            //    ModelYear = 2016
-            //});
-
-            //carManager.Delete(new Car
-            //{
-            //    CarId = 3,
-            //    BrandId = 2,
-            //    ColorId = 1,
-            //    Description = "Reno",
-            //    DailyPrice = 120,
-            //    ModelYear = 2016
-            //});
+            //CarAddUpdateDeleteTest(carManager);
 
             foreach (var car in carManager.GetCarDetails())
             {
                 //System.Console.WriteLine("Marka:{0} Model:{1} Kiralama Ücreti:{2}",car.Description,car.ModelYear,car.DailyPrice ); //GetAll için
                 System.Console.WriteLine("Araba Adı:{0} / Marka Adı:{1} / Renk Adı:{2} / Kiralama Ücreti:{3}",car.Description,car.BrandName,car.ColorName,car.DailyPrice);
             }
+        }
+
+        private static void CarAddUpdateDeleteTest(CarManager carManager)
+        {
+            carManager.Add(new Car
+            {
+                BrandId = 2,
+                ColorId = 3,
+                Description = "Fiat",
+                DailyPrice = 120,
+                ModelYear = 2015
+            });
+
+            carManager.Update(new Car
+            {
+                CarId = 3,
+                BrandId = 2,
+                ColorId = 1,
+                Description = "Reno",
+                DailyPrice = 120,
+                ModelYear = 2016
+            });
+
+            carManager.Delete(new Car
+            {
+                CarId = 3,
+                BrandId = 2,
+                ColorId = 1,
+                Description = "Reno",
+                DailyPrice = 120,
+                ModelYear = 2016
+            });
         }
     }
 }
