148237bf443cdc2b8ef52db3abc676c999251313
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-02-14 14:17:36 +0300

    14.02.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,6 +32,8 @@ namespace WebAPI
             services.AddControllers();
             services.AddSingleton<ICarService, CarManager>();
             services.AddSingleton<ICarDal, EfCarDal>();
+            services.AddSingleton<IBrandService, BrandManager>();
+            services.AddSingleton<IBrandDal, EfBrandDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
