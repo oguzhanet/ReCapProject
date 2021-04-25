2bcb442dc1bf0815d2163f490dc9c9064eea6b03
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-04-25 16:04:52 +0300

    25.04.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -73,7 +73,6 @@ namespace WebAPI
             {
                 new CoreModule()
             });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
